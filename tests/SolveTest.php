5b7c6a2f107dac2beffed833d25869778d8f7b38
MasyaSm <kuvalda-max@mail.ru>
2025-04-13 17:34:00 +0300

    добавлен базовый тест

---
+++ tests/SolveTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SolveTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testSanity()
+    {
+        $this->assertTrue(true);
+    }
+}
